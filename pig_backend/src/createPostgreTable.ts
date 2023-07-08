ead0020b3f8128910e6c92e551287bc70cf12323
Agustin Galarza <agalarza@itba.edu.ar>
2023-07-08 19:48:46 -0300

    Fixed problem with services startup.

--- pig_backend/src/createPostgreTable.ts
+++ pig_backend/src/createPostgreTable.ts
@@ -1,28 +1,28 @@
-import Pool from "pg-pool";
-import pg from "pg";
+import Pool from 'pg-pool';
+import pg from 'pg';
 
 export default function createPostgreTable(pool: Pool<pg.Client>) {
-  const query = `
-    CREATE TABLE users (
+	const query = `
+    CREATE IF NOT EXISTS TABLE users (
         nombre VARCHAR(255) NOT NULL,
-        uuid TEXT(36) PRIMARY KEY,
+        uuid CHAR(36) PRIMARY KEY,
         email VARCHAR(255) NOT NULL,
-        phone TEXT(10) NOT NULL,
-        cbu TEXT(22) NOT NULL,
+        phone CHAR(10) NOT NULL,
+        cbu CHAR(22) NOT NULL,
         secret_token VARCHAR(255) NOT NULL,
         alias VARCHAR(20) NOT NULL,
         creation_date TIMESTAMP NOT NULL,
     
         CONSTRAINT cbu_unique UNIQUE (cbu)
     );`;
-  pool
-    .query(query)
-    .then((res) => {
-      console.log("Table created successfully");
-      pool.end();
-    })
-    .catch((err) => {
-      console.error(err);
-      pool.end();
-    });
+	pool
+		.query(query)
+		.then(res => {
+			console.log('Table created successfully');
+			pool.end();
+		})
+		.catch(err => {
+			console.error(err);
+			pool.end();
+		});
 }
--- pig_backend/src/index.ts
+++ pig_backend/src/index.ts
@@ -1,66 +1,68 @@
-import express, { Request, Response } from "express";
-import amqp from "amqplib";
-import { v4 as uuidv4 } from "uuid";
-import Pool from "pg-pool";
-import createPostgreTable from "./createPostgreTable.ts";
+import express, { Request, Response } from 'express';
+import amqp from 'amqplib';
+import { v4 as uuidv4 } from 'uuid';
+import Pool from 'pg-pool';
+import createPostgreTable from './createPostgreTable.ts';
 
 const app = express();
 const port = 3000;
 
 const pool = new Pool({
-  user: "usuario",
-  password: "123",
-  host: "localhost",
-  port: 5432,
-  database: "pig_backend_users",
+	user: 'usuario',
+	password: '123',
+	host: 'postgres',
+	port: 5432,
+	database: 'pig_backend_users',
 });
 
 createPostgreTable(pool);
 
-const amqpConnection = await amqp.connect("amqp://localhost");
+await new Promise(r => setTimeout(r, 12 * 1000));
+
+const amqpConnection = await amqp.connect('amqp://rabbitmq:5672');
 const channel = await amqpConnection.createChannel();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(JSON.parse('{"message": "Hello Worlssdss!"}'));
+app.get('/', (req: Request, res: Response) => {
+	res.send(JSON.parse('{"message": "Hello Worlssdss!"}'));
 });
 
-app.post("/users", async (req, res) => {
-  try {
-    const { nombre, email, phone, cbu, secret_token, alias } = req.body;
-    const uuid = uuidv4();
-    const creation_date = new Date();
-    const query = `
+app.post('/users', async (req, res) => {
+	try {
+		const { nombre, email, phone, cbu, secret_token, alias } = req.body;
+		const uuid = uuidv4();
+		const creation_date = new Date();
+		const query = `
             INSERT INTO users (nombre, uuid, email, phone, cbu, secret_token, alias, creation_date)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING *;
         `;
-    await pool.query(query, [
-      nombre,
-      uuid,
-      email,
-      phone,
-      cbu,
-      secret_token,
-      alias,
-      creation_date,
-    ]);
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+		await pool.query(query, [
+			nombre,
+			uuid,
+			email,
+			phone,
+			cbu,
+			secret_token,
+			alias,
+			creation_date,
+		]);
+	} catch (error) {
+		console.error('Error creating user:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const db = req.app.locals.db;
-    const result = await db.query("SELECT * FROM users");
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+app.get('/users', async (req, res) => {
+	try {
+		const db = req.app.locals.db;
+		const result = await db.query('SELECT * FROM users');
+		res.json(result.rows);
+	} catch (error) {
+		console.error('Error fetching users:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
