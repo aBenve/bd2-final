f0d69081e89c0fc97c035f8f7b45f770d5b88a16
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2023-07-09 23:04:42 -0300

    add body parser to express

--- pig_backend/src/createPostgreTable.ts
+++ pig_backend/src/createPostgreTable.ts
@@ -1,7 +1,7 @@
 import Pool from "pg-pool";
 import pg from "pg";
 
-export default function createPostgreTable(pool: Pool<pg.Client>) {
+export function createPostgreTable(pool: Pool<pg.Client>) {
   const query = `
     CREATE TABLE IF NOT EXISTS users (
         nombre VARCHAR(255) NOT NULL,
@@ -21,7 +21,16 @@ export default function createPostgreTable(pool: Pool<pg.Client>) {
       console.log("Table created successfully");
     })
     .catch((err) => {
+      console.log("Error creating table");
       console.error(err);
       pool.end();
     });
+}
+
+export function addUserToDB(pool: Pool<pg.Client>) {
+  const query = `
+    INSERT INTO users (nombre, uuid, email, phone, cbu, secret_token, alias, creation_date)
+    VALUES ('Juan Perez', '123e4567-e89b-12d3-a456-426614174000', "", '1234567890', '1234567890123456789012', '123456', 'Juan', '2020-01-01 00:00:00');
+  `;
+  pool.query(query);
 }
