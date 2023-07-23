c478524c7d2009ca93a2b9b3af3910d17689620c
Agustin Galarza <agalarza@itba.edu.ar>
2023-07-23 18:48:44 -0300

    Updated Readme and corrected Postgre to Postgres

--- banco_postgres/src/main/java/ar/edu/itba/bd2/pig/bluebank/repository/TransactionHistoryRepository.java
+++ banco_postgres/src/main/java/ar/edu/itba/bd2/pig/bluebank/repository/TransactionHistoryRepository.java
@@ -2,8 +2,10 @@ package ar.edu.itba.bd2.pig.bluebank.repository;
 
 import ar.edu.itba.bd2.pig.bluebank.model.TransactionRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TransactionHistoryRepository extends JpaRepository<TransactionRecord, UUID> {
 }
