1638593d38bc94f0479b14df4f48f6e67c7d5ce5
Agustin-Galarza <67807012+Agustin-Galarza@users.noreply.github.com>
2023-07-24 04:57:29 +0000

    Fixed problem with default user creation in pig backend

--- pig_next/service/postgre.ts
+++ pig_next/service/postgre.ts
@@ -42,14 +42,15 @@ class PostgreClient {
       ?.query(
         `
       INSERT INTO users (name, uuid, email, phone, cbu, secret_token, alias, creation_date)
-      VALUES ('Juan Perez', '123e4567-e89b-12d3-a456-426614174000', 'juanperez@hotmail.com', '1234567890', '1234567890123456789012', '123456', 'Juan', '2020-01-01 00:00:00'),
-      ('Taylor Swift', '123e4567-e89b-12d3-a456-426614174001', '', '1234567890', '0000000000000000000001', '7395f2b7-d338-4770-bdbf-b7d4f9043f4c', 'Taylor', '2020-01-01 00:00:00'),
-      ('Carlos Menem', '123e4567-e89b-12d3-a456-426614174002', '', '1234567890', '0000000000000000000000', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Turco', '2020-01-01 00:00:00') 
+      VALUES 
+      ('Juan Perez', '123e4567-e89b-12d3-a456-426614174000', 'juanperez@hotmail.com', '1234567890', '1234567890123456789012', '123456', 'Juan', '2020-01-01 00:00:00'),
+      ('Taylor Swift', '123e4567-e89b-12d3-a456-426614174001', 'elturco@yahoo.com.ar', '0111234567', '0000000000000000000001', '7395f2b7-d338-4770-bdbf-b7d4f9043f4c', 'Taylor', '2020-01-01 00:00:00'),
+      ('Carlos Menem', '123e4567-e89b-12d3-a456-426614174002', 'swiftie001@gmail.com', '0117654321', '0000000000000000000000', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Turco', '2020-01-01 00:00:00') 
       ON CONFLICT DO NOTHING;
     `
       )
       .then((res) => {
-        console.log("User added successfully");
+        console.log("User added successfully", res.rowCount);
       })
       .catch((err) => {
         console.log("Error adding user");
